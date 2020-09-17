0f250df1d4ecd045bff4b6fb86e0733c04ea0af8
skyred-e <chocomint.lover@gmil.com>
2020-09-17 19:16:48 +0900

    Updates

--- precache-manifest.92457009ef489cdc3ac315f2c30ba412.js
+++ precache-manifest.92457009ef489cdc3ac315f2c30ba412.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa1c9811d00ab180cd5a35b53a588ec8",
+    "revision": "e059335bc1a8fc98cadfe4b190e3fa62",
     "url": "/anmc/index.html"
   },
   {
-    "revision": "5429de23aed884c54b30",
+    "revision": "366c9a1b8eb143d6f2eb",
     "url": "/anmc/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/anmc/static/js/2.b787fc57.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5429de23aed884c54b30",
-    "url": "/anmc/static/js/main.4ffa0ee7.chunk.js"
+    "revision": "366c9a1b8eb143d6f2eb",
+    "url": "/anmc/static/js/main.a609e15b.chunk.js"
   },
   {
     "revision": "11755150542e8e6e1998",
